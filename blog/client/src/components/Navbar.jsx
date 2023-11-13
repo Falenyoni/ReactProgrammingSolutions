5b588a706e1b8de6d8091f41c75b8bbd8ae2d952
Bongani Nyoni <b.nyoni1101@gmail.com>
2023-11-13 09:52:43 +0200

    Adding All Updated Blog Files

--- blog/client/src/components/Navbar.jsx
+++ blog/client/src/components/Navbar.jsx
@@ -1,39 +1,52 @@
-import React from 'react'
-import Logo from "../img/logo.png"
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import Logo from "../img/logo.png";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
 
-function Navbar() {
+const Navbar = () => {
+  const { currentUser, logout } = useContext(AuthContext);
+
   return (
-    <div className='navbar'>
-        <div className="container">
-            <div className="logo">
-                <img src={Logo} alt="" />
-            </div>
-            <div className="links">
-                <Link className='link' to='/?cart=coding'>
-                   <h6>CODING</h6> 
-                </Link>
-                <Link className='link' to='/?cart=interviews'>
-                   <h6>INTERVIEWS</h6> 
-                </Link>
-                <Link className='link' to='/?cart=cloud'>
-                   <h6>CLOUD</h6> 
-                </Link>
-                <Link className='link' to='/?cart=religion'>
-                   <h6>RELIGION</h6> 
-                </Link>
-                <Link className='link' to='/?cart=art'>
-                   <h6>ART</h6> 
-                </Link>
-                <span>Bongani</span>
-                <span>Logout</span>
-                <span className='write'>
-                    <Link className='link' to="/write">Write</Link>
-                </span>
-            </div>
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+        <Link className="link" to="/">
+        <img src={Logo} alt="" />
+        </Link>         
         </div>
+        <div className="links">
+          <Link className="link" to="/?cat=coding">
+            <h6>CODING</h6>
+          </Link>
+          <Link className="link" to="/?cat=interviews">
+            <h6>INTERVIEWS</h6>
+          </Link>
+          <Link className="link" to="/?cat=cloud">
+            <h6>CLOUD</h6>
+          </Link>
+          <Link className="link" to="/?cat=religion">
+            <h6>RELIGION</h6>
+          </Link>
+          <Link className="link" to="/?cat=art">
+            <h6>ART</h6>
+          </Link>
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- blog/client/src/pages/Single.jsx
+++ blog/client/src/pages/Single.jsx
@@ -1,51 +1,76 @@
-import React from 'react'
-import Edit from '../img/edit.png'
-import Delete from '../img/delete.png'
-import { Link } from "react-router-dom";
-import Menu from '../components/Menu';
+import React, { useContext, useEffect, useState } from "react";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import Menu from "../components/Menu";
+import axios from "axios";
+import moment from "moment";
+import { AuthContext } from "../context/authContext";
 
 const Single = () => {
-  return (
-    <div className='single'>
-      <div className="content">
-        <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
-        <div className="user">
-          <img src="" alt="" />
-          <div className="info">
-            <span>Bongani</span>
-            <p>Posted 2 days ago</p>
-          </div>
-          <div className="edit">
-            <Link className='link' to={`/write?edit=2`}>
-              <img src={Edit} alt="" />
-            </Link>            
-            <img src={Delete} alt="" />
-          </div>
-        </div>
-        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
-        <p>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.  Quod, quam. Vel in magnam dolerem,
-            delectus, quasi expedita eviniet distinctio architecto, laboriosam blanditiis aliquid. Enim, itaque mollitia. Vero, sunt. Exercitationem, auttem?
-            <br /><br />
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.  Quod, quam. Vel in magnam dolerem, delectus, quasi expedita eviniet distinctio architecto, laboriosam blanditiis aliquid.
-            Enim, itaque mollitia. Vero, sunt. Exercitationem, auttem?
-            <br /><br />
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.  Quod, quam. Vel in magnam dolerem, delectus, quasi expedita eviniet distinctio architecto, laboriosam blanditiis aliquid.
-            Enim, itaque mollitia. Vero, sunt. Exercitationem, auttem? Lorem, ipsum dolor sit amet consectetur adipisicing elit.  Quod, quam. Vel in magnam dolerem, delectus, quasi expedita eviniet distinctio architecto, 
-            laboriosam blanditiis aliquid. Enim, itaque mollitia. Vero, sunt. Exercitationem, auttem?
-            <br /><br />
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.  Quod, quam. Vel in magnam dolerem, delectus, quasi expedita eviniet distinctio architecto, laboriosam blanditiis aliquid. 
-            Enim, itaque mollitia. Vero, sunt. Exercitationem, auttem? Lorem, ipsum dolor sit amet consectetur adipisicing elit.  Quod, quam. Vel in magnam dolerem, delectus, quasi expedita eviniet distinctio architecto, 
-            laboriosam blanditiis aliquid. Enim, itaque mollitia. Vero, sunt. Exercitationem, auttem? Lorem, ipsum dolor sit amet consectetur adipisicing elit.  Quod, quam. Vel in magnam dolerem, delectus, quasi expedita eviniet 
-            distinctio architecto, laboriosam blanditiis aliquid. Enim, itaque mollitia. Vero, sunt. Exercitationem, auttem?  Lorem, ipsum dolor sit amet consectetur adipisicing elit.  Quod, quam. Vel in magnam dolerem,
-            delectus, quasi expedita eviniet distinctio architecto, laboriosam blanditiis aliquid. Enim, itaque mollitia. Vero, sunt. Exercitationem, auttem?
-          </p>
-        </p>
-      </div>
-      <Menu />
-    </div>
-  )
-}
+  const [post, setPost] = useState({});
 
-export default Single
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const postId = location.pathname.split("/")[2];
+
+  const { currentUser } = useContext(AuthContext);
+
+  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [postId]);
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${postId}`);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getText = (html) =>{
+    const doc = new DOMParser().parseFromString(html, "text/html")
+    return doc.body.textContent
+  }
+  
+
+  return (
+    <div className="single">
+      <div className="content">
+        <img src={`../upload/${post?.img}`} alt="" />
+        <div className="user">
+          {post.userImg && <img src={post.userImg} alt="" />}
+          <div className="info">
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
+          </div>
+          {currentUser.username === post.username && (
+            <div className="edit">
+              <Link  to={`/write?edit=2`} state={post}>
+                <img src={Edit} alt="" />
+              </Link>
+              <img onClick={handleDelete} src={Delete} alt="" />
+            </div>
+          )}
+        </div>
+        <h1>{post.title}</h1>
+        {getText(post.desc)}
+      </div>
+      <Menu cat={post.cat} />
+    </div>
+  );
+};
+
+export default Single;
